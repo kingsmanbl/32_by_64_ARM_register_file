9712fcff82f4df33098a663f58df69ca46ed21eb
kingsmanbl <bernardolinbl@gmail.com>
2025-06-15 15:28:00 +0800

    fixed 3:8 decoder so that it outputs corresponds to the inputs correctly.

--- three_to_eight_decoder.sv
+++ three_to_eight_decoder.sv
@@ -10,13 +10,13 @@ module three_to_eight_decoder(in, out, enable);
 	not #(50) notin1(not1, in[1]);
 	not #(50) notin2(not2, in[2]);
 	
-	and #(50) num0 (out[0], not2, not1, not0, enable);
-	and #(50) num1 (out[1], not2, not1, in[0], enable);
-	and #(50) num2 (out[2], not2, in[1], not0, enable);
-	and #(50) num3 (out[3], not2, in[1], in[0], enable);
-	and #(50) num4 (out[4], in[2], not1, not0, enable);
-	and #(50) num5 (out[5], in[2], not1, in[0], enable);
-	and #(50) num6 (out[6], in[2], in[1], not0, enable);
-	and #(50) num7 (out[7], in[2], in[1], in[0], enable);
+	and #(50) num0 (out[0], not0, not1, not2, enable);
+	and #(50) num1 (out[1], in[0], not1, not2, enable);
+	and #(50) num2 (out[2], not0, in[1], not2, enable);
+	and #(50) num3 (out[3], in[0], in[1], not2, enable);
+	and #(50) num4 (out[4], not0, not1, in[2], enable);
+	and #(50) num5 (out[5], in[0], not1, in[2], enable);
+	and #(50) num6 (out[6], not0, in[1],in[2], enable);
+	and #(50) num7 (out[7], in[0], in[1], in[2], enable);
 	
 endmodule
